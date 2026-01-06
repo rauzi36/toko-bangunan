5530a0a6d3c4c391ea5ad746b1c9274a4f5b2fa5
rauzi <rauzicity@gmail.com>
2026-01-06 19:55:57 +0700

    Fix static path absolute

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // Middleware
 app.use(express.json());
-app.use(express.static('public')); 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
     secret: 'rahasia_toko_bangunan',
     resave: false,
